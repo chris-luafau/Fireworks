791403d71224e366fa45d1f85714922192743ca2
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-10-23 23:44:02 -0700

    Setup Premake file.

---
+++ premake5.lua
@@ -0,0 +1,98 @@
+
+workspace "Fireworks"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Fireworks"
+	location "Fireworks"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"FZ_PLATFORM_WINDOWS",
+			"FZ_BUILD_DLL"
+		}
+		
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "FZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "FZ_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Fireworks/vendor/spdlog/include",
+		"Fireworks/src"
+	}
+
+	links {
+		"Fireworks"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"FZ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "FZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "FZ_DIST"
+		optimize "On"
