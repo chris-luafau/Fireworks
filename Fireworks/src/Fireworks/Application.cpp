d871d89d0abaf3ca891b1417d6347e1d7221afed
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-10-27 00:33:27 -0700

    Added a quick test to check that GLFW is working. Window should open and be pink.

--- Fireworks/src/Fireworks/Application.cpp
+++ Fireworks/src/Fireworks/Application.cpp
@@ -4,6 +4,8 @@
 #include "Fireworks/Events/ApplicationEvent.h"
 #include "Fireworks/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Fireworks {
 
 	Application::Application() {
@@ -16,6 +18,9 @@ namespace Fireworks {
 
 	void Application::Run() {
 		while (m_Running) {
+			// Testing GLFW.
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
