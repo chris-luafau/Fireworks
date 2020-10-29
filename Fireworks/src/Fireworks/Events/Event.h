430d4bbb07b568c91640f6497fdae57c33ba7f94
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-10-28 20:22:01 -0700

    Setup simple layer stack.

--- Fireworks/src/Fireworks/Events/Event.h
+++ Fireworks/src/Fireworks/Events/Event.h
@@ -37,7 +37,10 @@ namespace Fireworks {
 
 	class FIREWORKS_API Event {
 		friend class EventDispatcher;
+
 	public:
+		bool Handled = false;
+
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
@@ -46,8 +49,6 @@ namespace Fireworks {
 		inline bool IsInCategory(EventCategory category) {
 			return GetCategoryFlags() & category;
 		}
-	protected:
-		bool m_Handled = false;
 	};
 
 	class EventDispatcher {
@@ -62,7 +63,7 @@ namespace Fireworks {
 		template<typename T>
 		bool Dispatch(EventFn<T> func) {
 			if (m_Event.GetEventType() == T::GetStaticType()) {
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
 			}
 			return false;
---
+++ Fireworks/src/Fireworks/Layer.cpp
@@ -0,0 +1,14 @@
+#include "fzpch.h"
+#include "Layer.h"
+
+
+namespace Fireworks {
+
+	Layer::Layer(const std::string& debugName)
+		: m_DebugName(debugName) {
+
+	}
+
+	Layer::~Layer() {}
+
+}
