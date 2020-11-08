665df974226277067bd230bf5fc29de10707752a
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-11-07 21:50:49 -0800

    Draw a triangle using OpenGL.

--- Fireworks/src/Fireworks/Application.cpp
+++ Fireworks/src/Fireworks/Application.cpp
@@ -21,6 +21,42 @@ namespace Fireworks {
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		// To render a triangle, we need:
+		// Vertex Array
+		// Vertex Buffer
+		// Index buffer
+		// Shader --> For now, we will use the shaders provided by the GPU.
+
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+		float vertices[3 * 3] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f,  0.5f, 0.0f
+		};
+
+		// Upload vertices to the GPU.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		// Tell OpenGL the layout/format of the data.
+		// I.e. OpenGL doesn't know that the vertices data is a 3x3 matrix.
+		// It just sees them as bytes of data.
+		glEnableVertexAttribArray(0);
+		// Start at index 0, there are 3 items, they're floats, won't be normalized, reserve space, and there is no offset between
+		// multiple attributes (we only have 1 attribute in this case - vertices).
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		glGenBuffers(1, &m_IndexBuffer);
+		// OpenGL refers to index buffers as element buffers.
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		unsigned int indices[3] = { 0, 1, 2 };
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	}
 
 	Application::~Application() {
@@ -50,9 +86,12 @@ namespace Fireworks {
 
 	void Application::Run() {
 		while (m_Running) {
-			// Testing GLFW.
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			// Bind the vertex array and draw.
+			glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 			m_ImGuiLayer->Begin(); 
 			for (Layer* layer : m_LayerStack)
