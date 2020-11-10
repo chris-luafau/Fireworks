1512f663dcecd5e9cc0d174d84c1df726db8e86d
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-11-09 22:15:34 -0800

    Fixed wrong spdlog api call.

--- Fireworks/src/Fireworks/Log.h
+++ Fireworks/src/Fireworks/Log.h
@@ -24,11 +24,11 @@ namespace Fireworks {
 #define FZ_CORE_INFO(...)  ::Fireworks::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define FZ_CORE_WARN(...)  ::Fireworks::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define FZ_CORE_ERROR(...) ::Fireworks::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define FZ_CORE_FATAL(...) ::Fireworks::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define FZ_CORE_FATAL(...) ::Fireworks::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define FZ_TRACE(...)      ::Fireworks::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define FZ_INFO(...)       ::Fireworks::Log::GetClientLogger()->info(__VA_ARGS__)
 #define FZ_WARN(...)       ::Fireworks::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define FZ_ERROR(...)      ::Fireworks::Log::GetClientLogger()->error(__VA_ARGS__)
-#define FZ_FATAL(...)      ::Fireworks::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define FZ_FATAL(...)      ::Fireworks::Log::GetClientLogger()->critical(__VA_ARGS__)
