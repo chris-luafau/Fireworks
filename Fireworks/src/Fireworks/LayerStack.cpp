f2f59d2ecea0a745970b07e7f344585cbd88489d
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-11-12 21:39:38 -0800

    Detach layer before removing from stack.

--- Fireworks/src/Fireworks/LayerStack.cpp
+++ Fireworks/src/Fireworks/LayerStack.cpp
@@ -24,6 +24,7 @@ namespace Fireworks {
 	void LayerStack::PopLayer(Layer* layer) {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if (it != m_Layers.end()) {
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
 		}
@@ -31,7 +32,9 @@ namespace Fireworks {
 
 	void LayerStack::PopOverlay(Layer* overlay) {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
-		if (it != m_Layers.end())
+		if (it != m_Layers.end()) {
+			overlay->OnDetach();
 			m_Layers.erase(it);
+		}
 	}
 }
