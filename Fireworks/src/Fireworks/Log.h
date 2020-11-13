ac7f7aefebcc9197334507525952f2490da5faf7
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-11-12 21:37:20 -0800

    Changed macro name to match spdlog.

--- Fireworks/src/Fireworks/Log.h
+++ Fireworks/src/Fireworks/Log.h
@@ -21,15 +21,15 @@ namespace Fireworks {
 }
 
 // Core log macros
-#define FZ_CORE_TRACE(...) ::Fireworks::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define FZ_CORE_INFO(...)  ::Fireworks::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define FZ_CORE_WARN(...)  ::Fireworks::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define FZ_CORE_ERROR(...) ::Fireworks::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define FZ_CORE_FATAL(...) ::Fireworks::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define FZ_CORE_TRACE(...)    ::Fireworks::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define FZ_CORE_INFO(...)     ::Fireworks::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define FZ_CORE_WARN(...)     ::Fireworks::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define FZ_CORE_ERROR(...)    ::Fireworks::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define FZ_CORE_CRITICAL(...) ::Fireworks::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
-#define FZ_TRACE(...)      ::Fireworks::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define FZ_INFO(...)       ::Fireworks::Log::GetClientLogger()->info(__VA_ARGS__)
-#define FZ_WARN(...)       ::Fireworks::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define FZ_ERROR(...)      ::Fireworks::Log::GetClientLogger()->error(__VA_ARGS__)
-#define FZ_FATAL(...)      ::Fireworks::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define FZ_TRACE(...)         ::Fireworks::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define FZ_INFO(...)          ::Fireworks::Log::GetClientLogger()->info(__VA_ARGS__)
+#define FZ_WARN(...)          ::Fireworks::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define FZ_ERROR(...)         ::Fireworks::Log::GetClientLogger()->error(__VA_ARGS__)
+#define FZ_CRITICAL(...)      ::Fireworks::Log::GetClientLogger()->critical(__VA_ARGS__)
