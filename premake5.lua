a932cffd4f12d1aff9a66016d31dd56a3134d827
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-12-04 22:40:01 -0800

    Add flag to enable VS to use multi-compiler compilation.

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,10 @@ workspace "Fireworks"
 		"Dist"
 	}
 
+	flags {
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory).
